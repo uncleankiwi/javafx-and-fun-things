dab5c8a8bb640e17a89434c3b887a24e3006bbaa
uncleankiwi <feldtspirit@gmail.com>
2021-05-14 08:27:58 -0400

    rtg

--- src/jfx/RichTextGradient.java
+++ src/jfx/RichTextGradient.java
@@ -2,8 +2,18 @@ package jfx;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ColorPicker;
+import javafx.scene.control.RadioButton;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
 import javafx.stage.Stage;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /*
 primaryStage
@@ -13,10 +23,11 @@ primaryStage
 				txtInput
 				txtOutput	- textflow
 			rtgWrapperRight
-				lstColours
-					btnAddColour
-					btnRemoveColour
-					colorPicker	- ColorPicker
+				lstColours - ColourList
+					colorItem - ColorItem
+						btnAddColour
+						btnRemoveColour
+						colorPicker	- ColorPicker
 				radRGB
 				radHSB
 				btnColour
@@ -39,9 +50,74 @@ public class RichTextGradient extends Application {
 }
 
 class RtgUI extends HBox {
+	Text txtInput;
+	TextFlow txtOutput;
+	ColourList colourList;
+	RadioButton radRGB;
+	RadioButton radHSB;
 
 
 	RtgUI () {
 
 	}
+}
+
+class ColourList extends VBox {
+	List<ColourItem> colourItems;
+
+	ColourList() {
+		colourItems = new ArrayList<>();
+
+		addColour(0);
+	}
+
+	//adds it 1 after the index if the list isn't currently empty
+	void addColour(int index) {
+		if (this.colourItems.size() == 0) index = 0;
+		else index++;
+
+		ColourItem colourItem = new ColourItem(this, index);
+		colourItems.add(colourItem);
+
+		refresh();
+	}
+
+	//always leave at least 1 colour in the list
+	void removeColour(int index) {
+		if (this.colourItems.size() <= 1) return;
+		else colourItems.remove(index);
+
+		refresh();
+	}
+
+	private void refresh() {
+		this.getChildren().clear();
+		this.getChildren().addAll(this.colourItems);
+	}
+
+}
+
+class ColourItem extends HBox {
+	Button btnAddColour = new Button();
+	Button btnRemoveColour = new Button();
+	ColorPicker colourPicker = new ColorPicker();
+	ColourList parent;
+	Color colour;
+	int index;
+
+	ColourItem(ColourList parent, int index) {
+		this.index = index;
+		this.parent = parent;
+
+		this.colour = Color.BLACK;
+		colourPicker.setValue(this.colour);
+
+		btnAddColour.setOnAction(event -> parent.addColour(this.index));
+		btnRemoveColour.setOnAction(event -> parent.removeColour(this.index));
+		colourPicker.setOnAction(event -> this.colour = colourPicker.getValue());
+
+		this.getChildren().addAll(btnAddColour, btnRemoveColour, colourPicker);
+	}
+
+
 }
