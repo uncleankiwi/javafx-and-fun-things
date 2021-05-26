c6072dde3e400e241254a5dd593dc7802ca19188
uncleankiwi <feldtspirit@gmail.com>
2021-05-26 10:34:15 -0400

    RollDiameter done

--- src/misc/eda/RollDiameter.java
+++ src/misc/eda/RollDiameter.java
@@ -12,21 +12,30 @@ public class RollDiameter {
 		test(50, 0.025, 4, 4);		//Expected answer: 4.02
 		test(4321, 0.025, 4, 4);	//Expected answer: 5.4575
 		test(10000, 0.025, 4, 4);	//Expected answer: 6.9175
+		test(123456, 0.025, 4, 4); 	//Expected answer: 20.2275
 	}
 
 	public static double calculateRollDiameter(double length, double thickness, double diameter, int places) {
 		//resulting diameter has to be incremented twice per round; once when it reaches where the material starts,
 		//and again when material reaches the opposite side
 		boolean justIncrementedDiameter = true;
+		//diameter not including outermost layer. used to calculate length consumed in outer-most layer
+		double innerDiameter = diameter;
+		//diameter including outermost layer
+		double outerDiameter = diameter;
 		while (length > 0) {
-			length -= 0.5 * Math.PI * Math.pow(diameter / 2, 2);
+			//length -= half a round
+			//0.5 * 2 * pi * r = pi * d / 2
+			length -= Math.PI * innerDiameter / 2;
+			outerDiameter += thickness / 10;
 			if (!justIncrementedDiameter) {
-				diameter += thickness / 1000;
+				innerDiameter += 2 * thickness / 10;
 				justIncrementedDiameter = true;
 			}
 			else justIncrementedDiameter = false;
 		}
-		return diameter;
+
+		return Double.parseDouble(String.format("%." + places + "f", outerDiameter)); //"%." + places + "f"
 	}
 
 	public static void test(double length, double thickness, double diameter, int places) {
