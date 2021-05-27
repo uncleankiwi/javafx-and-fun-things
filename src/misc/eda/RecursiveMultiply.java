f5e273a58cf27e8c7c33027de12aa85a3b734b09
uncleankiwi <feldtspirit@gmail.com>
2021-05-27 10:38:40 -0400

    RecursiveMultiply

--- src/misc/eda/RecursiveMultiply.java
+++ src/misc/eda/RecursiveMultiply.java
@@ -1,20 +1,71 @@
 package misc.eda;
 
-//Given a string, multiply the integer it represents by 11 without casting the string as a number.
+//Given a string, multiply the integer it represents by a given integer without casting the string as a number.
 //Return the result as a string.
+//Extra limitations: use recursion and don't use arrays/lists.
 public class RecursiveMultiply {
 	public static void main(String[] args) {
-		test("11");								//expected: 121
-		test("111111111");						//expected: 1222222221
-		test("9473745364784876253253263723");		//expected: 104211199012633638785785900953
+		test("11", 11);								//expected: 121
+		test("111111111", 11);						//expected: 1222222221
+		test("9473745364784876253253263723", 11);		//expected: 104211199012633638785785900953
 
 	}
 
-	public static String recursiveMultiply11(String input) {
+	public static String recursiveMultiply(String input, int number) {
+		String rightPart = String.valueOf(numberAt(input, input.length() - 1) * number);
+		String result = rightPart;
+		if (input.length() != 1) {
+			String leftPart = recursiveMultiply(input.substring(0, input.length() - 1), number) + "0";
+			//creating a third string for carried numbers
+			//it should be 1 '0' longer than the left part since the left-most digit could also get carried
+			StringBuilder carryBuilder = new StringBuilder("0");
+			for (int i = 0; i < leftPart.length(); i++) {
+				carryBuilder.append("0");
+			}
+			String carry = carryBuilder.toString();
+			String addedResult = carry;
 
+			//adding left, right, carry strings and putting into addedResult
+			for (int i = 0; i < addedResult.length(); i++) {
+				int numberLeft = 0;
+				if (leftPart.length() - 1 - i > 0) {
+					numberLeft = numberAt(leftPart, leftPart.length() - 1 - i);
+				}
+				int numberRight = 0;
+				if (rightPart.length() - 1 - i > 0) {
+					numberRight = numberAt(rightPart, rightPart.length() - 1 - i);
+				}
+				int numberCarry = numberAt(carry, carry.length() - 1 - i);
+				int localResult = numberLeft + numberRight + numberCarry;
+
+				//putting into addedResult
+				String localResultString = String.valueOf(localResult);
+				addedResult = setNumberAt(addedResult, addedResult.length() - 1 - i,
+					numberAt(localResultString, localResultString.length() - 1));
+
+				//carry if necessary
+				if (localResultString.length() > 1) {
+					carry = setNumberAt(carry, carry.length() - 2 - i,
+						numberAt(localResultString, 0));
+				}
+			}
+
+			result = addedResult;
+		}
+		return result;
 	}
 
-	public static void test(String input) {
-		System.out.println(input + " * 11 = " + recursiveMultiply11(input));
+	public static void test(String input, int number) {
+		System.out.println(input + " * " + number + " = " + recursiveMultiply(input, number));
+	}
+
+	private static int numberAt(String string, int index) {
+		return Integer.parseInt(String.valueOf(string.charAt(index)));
+	}
+
+	//sets the number at index position in string to a new number
+	//TODO test if return needed?
+	private static String setNumberAt(String string, int index, int number) {
+
 	}
 }
