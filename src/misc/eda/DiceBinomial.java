dbf11223f1938595fd79cababbc6ccadc2d4ecc6
uncleankiwi <feldtspirit@gmail.com>
2021-06-06 11:32:33 -0400

    DiceBinomial

---
+++ src/misc/eda/DiceBinomial.java
@@ -0,0 +1,11 @@
+package misc.eda;
+/*
+Given a number of 6-sided dice d and a desired outcome o,
+	1. 	a. draw the binomial distribution of results, scaled to <= 30 characters
+		b. with each row labelled with the numerical result,
+		c. with an arrow indicating the row with the desired outcome.
+	2. state the probability of outcome o as a fraction.
+ */
+
+public class DiceBinomial {
+}
