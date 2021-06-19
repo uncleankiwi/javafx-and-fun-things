94e7644d97c38db83acfbd2be4e795f535859a1e
uncleankiwi <feldtspirit@gmail.com>
2021-06-19 05:26:06 -0400

    EstimateChart

--- src/pi_estimator/EstimateChart.java
+++ src/pi_estimator/EstimateChart.java
@@ -10,7 +10,16 @@ import javafx.stage.Stage;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Objects;
 
+/*
+Draws estimates grouped by number of digits on a javafx chart.
+Probably not ideal because it's too resource-intensive when fed
+a high digit number; every estimate is a separate node.
+
+The display also gets a little... funky at high digits due
+to over-crowding. Looks best at >=6 digits.
+ */
 public class EstimateChart extends Application {
 	public static void main(String[] args) {
 		launch();
@@ -27,10 +36,8 @@ public class EstimateChart extends Application {
 		final LineChart<Number, Number> chart = new LineChart<>(xAxis, yAxis);
 		chart.setTitle("Closeness of estimates of pi");
 
-
-
 		//getting lists of estimate from the generator
-		final int maxDigits = 4;
+		final int maxDigits = 6;
 		EstimateGenerator.populate(maxDigits);
 		List<Estimate> estimates = EstimateGenerator.get();
 		List<Estimate> bestEstimates = EstimateGenerator.getBest();
@@ -54,16 +61,19 @@ public class EstimateChart extends Application {
 		//while calculating x-axis value in-sito
 		for (int i = 0; i < maxDigits; i++) {	//for each digit band
 			XYChart.Series<Number, Number> series = new XYChart.Series<>();
+			series.setName((i + 1) + " digits");
 
 			List<Integer> bandCloseness = closeness.get(i);
 			for (int j = 0; j < bandCloseness.size(); j++) {	//for every different denominator
+
 				series.getData().add(new XYChart.Data<>(
-					(double) (j / (bandSize[i] - 1)), bandCloseness.get(j)));
+					((double) j / (bandSize[i] - 1)), bandCloseness.get(j)));
 			}
 
 			chart.getData().add(series);
 		}
 
+		chart.getStylesheets().add(Objects.requireNonNull(getClass().getResource("chart.css")).toExternalForm());
 		Scene scene = new Scene(chart, 800, 600);
 		stage.setScene(scene);
 		stage.show();
