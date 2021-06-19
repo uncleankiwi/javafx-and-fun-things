e7035932df0685d13b8983d9daa0fff5e1a37690
uncleankiwi <feldtspirit@gmail.com>
2021-06-19 04:45:03 -0400

    EstimateChart

--- src/pi_estimator/EstimateChart.java
+++ src/pi_estimator/EstimateChart.java
@@ -1,10 +1,14 @@
 package pi_estimator;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
 import javafx.stage.Stage;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class EstimateChart extends Application {
@@ -31,8 +35,37 @@ public class EstimateChart extends Application {
 		List<Estimate> estimates = EstimateGenerator.get();
 		List<Estimate> bestEstimates = EstimateGenerator.getBest();
 
-		//
+		//get the number of points in each digit band's estimates
+		//while also splitting each band into a separate list
+		final int[] bandSize = new int[maxDigits];
+		ArrayList<LinkedList<Integer>> closeness = new ArrayList<>();
 
+		for (int i = 0; i < maxDigits; i++) {
+			closeness.add(new LinkedList<>());
+		}
+
+		for (Estimate estimate : estimates) {
+			int digits = estimate.digits() - 1;
+			bandSize[digits]++;
+			closeness.get(digits).add(estimate.absoluteCloseness());
+		}
+
+		//assign bands to separate series, then populate those series
+		//while calculating x-axis value in-sito
+		for (int i = 0; i < maxDigits; i++) {	//for each digit band
+			XYChart.Series<Number, Number> series = new XYChart.Series<>();
+
+			List<Integer> bandCloseness = closeness.get(i);
+			for (int j = 0; j < bandCloseness.size(); j++) {	//for every different denominator
+				series.getData().add(new XYChart.Data<>(
+					(double) (j / (bandSize[i] - 1)), bandCloseness.get(j)));
+			}
+
+			chart.getData().add(series);
+		}
+
+		Scene scene = new Scene(chart, 800, 600);
+		stage.setScene(scene);
 		stage.show();
 	}
 
