e51cd4b9cd1eacfb607b28162aca98c3c9e2226f
uncleankiwi <feldtspirit@gmail.com>
2021-07-17 17:00:51 -0400

    CentrifugeBalancer: tests

--- src/misc/eda/CentrifugeBalancer.java
+++ src/misc/eda/CentrifugeBalancer.java
@@ -10,34 +10,43 @@ public class CentrifugeBalancer {
 	public static void main(String[] args) {
 		System.out.println("Holes\tTubes");
 		System.out.println("=============");
-		test(6, 4);	//true
-		test(2,1);	//false
-		test(3,3);	//true
-		test(5,1);	//false
-		test(12,7);	//true
-		test(1,1);	//false
-		test(21,18);	//true
-		test(1,0);	//false
-		test(4,2);	//true
-		test(5,3);	//false
-		test(21,13);	//false
-		test(3,3);	//true
-		test(50,1);	//false
-		test(8,6);	//true
-		test(9,5);	//false
-		test(2,1);	//false
-		test(59,59);	//true
-		test(11,4);	//false
+		test(6, 4, true);
+		test(2,1, false);
+		test(3,3, true);
+		test(5,1, false);
+		test(12,7, true);
+		test(1,1, false);
+		test(21,18, true);
+		test(1,0, false);
+		test(4,2, true);
+		test(5,3, false);
+		test(21,13, false);
+		test(3,3, true);
+		test(50,1, false);
+		test(8,6, true);
+		test(9,5, false);
+		test(2,1, false);
+		test(59,59, true);
+		test(11,4, false);
+		test(21,20, false);
+		test(24, 13, true);
 
 	}
 
-	private static void test(int h, int t) {
-		System.out.println(h + "\t" + t + "\t->\t" + tryPlace(h, t));
+	private static void test(int h, int t, boolean expectedOutput) {
+		boolean actualOutput = tryPlace(h, t);
+		String str = h + "\t" + t + "\t->\t" + actualOutput;
+		if (actualOutput != expectedOutput) str += " (TEST FAILED)";
+		System.out.println(str);
 	}
 
 	public static boolean tryPlace(int h, int t) {
+		//weed out obvious answers
 		if (h <= 1) return false;
 		else if (t <= 1) return false;
+		else if (t > h) return false;
+		else if (h == t) return true;
+
 		else {
 			return true;	//todo
 		}
