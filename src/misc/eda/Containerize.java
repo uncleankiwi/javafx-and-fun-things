a30d5c7a052a9faf37779e9c3d1c887abdf79dd9
uncleankiwi <feldtspirit@gmail.com>
2021-07-29 18:20:12 -0400

    Containerize

---
+++ src/misc/eda/Containerize.java
@@ -0,0 +1,26 @@
+package misc.eda;
+
+import java.util.List;
+
+/*
+Given a list of items of varying sizes, this does two different things:
+	1. tries to fit all items into n groups of fixed size s
+	2. tries to split the items into n groups of equal size
+ */
+public class Containerize {
+	public static void main(String[] args) {
+
+	}
+
+	//tries to fit the given items into containerCount containers of containerSize.
+	//returns null if it can't be fit.
+	public static List<List<Integer>> fit(List<Integer> items, int containerSize, int containerCount) {
+		return null; //todo
+	}
+
+	//splits the given items into containerCount groups of equal size
+	//returns null if it can't be split.
+	public static List<List<Integer>> split(List<Integer> items, int containerCount) {
+		return null; //todo
+	}
+}
