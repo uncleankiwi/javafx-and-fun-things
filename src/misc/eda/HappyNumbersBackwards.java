f2b31d74c8ae5c8c6439d95e3907c75220319ba5
uncleankiwi <feldtspirit@gmail.com>
2021-11-18 06:30:24 -0500

    HappyNumbersBackwards permutations

--- src/misc/eda/HappyNumbersBackwards.java
+++ src/misc/eda/HappyNumbersBackwards.java
@@ -1,9 +1,6 @@
 package misc.eda;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Objects;
-import java.util.Set;
+import java.util.*;
 
 /*
 	Generates happy numbers with unique digits within a range.
@@ -14,22 +11,40 @@ import java.util.Set;
 	Unlike the HappyNumbers class, this works backwards.
 	It takes the base case, 1, and works out all the 1 to n digit numbers that resolve to that,
 	until it reaches the point where all numbers obtained are already in the set of answers.
+
+	See SumToNumber class for a simplified version of the logic used to generate a set of n numbers that
+	sum to a given number.
  */
 public class HappyNumbersBackwards {
 	public static void main(String[] args) {
+		testPermutations(253);
+		testPermutations(2552);
+		testPermutations(13);
+	}
 
+	private static void testPermutations(int n) {
+		UniqueNumber x = new UniqueNumber(n);
+		for (int i = 1; i <= 5; i++) {
+			System.out.println("unique number " + n + ": " + i + " digits:" + x.permutationsWrapper(i));
+		}
 	}
 
 	private static class UniqueNumber {
 		private final Map<Character, Integer> digits;
+		private int size = 0;
 
 		UniqueNumber(int n) {
 			digits = new HashMap<>();
 			for (char c : String.valueOf(n).toCharArray()) {
-				if (c != '0') {
-					if (digits.containsKey(c)) digits.replace(c, digits.get(c));
-					else digits.put(c, 1);
-				}
+				add(this.digits, c, false);
+			}
+		}
+
+		private void add(Map<Character, Integer> tally, char c, boolean addZeroes) {
+			if (c != '0' || addZeroes) {
+				size++;
+				if (tally.containsKey(c)) tally.replace(c, tally.get(c));
+				else tally.put(c, 1);
 			}
 		}
 
@@ -41,12 +56,51 @@ public class HappyNumbersBackwards {
 			return null;
 		}
 
+		//wrapper for recursive method
+		private Set<Integer> permutationsWrapper(int size) {
+			if (this.size > size) return new HashSet<>();
+			Map<Character, Integer> digitsCopy = new HashMap<>(digits);
+			for (int i = 1; i <= size - this.size; i++) {
+				add(digitsCopy, '0', true);
+			}
+			Set<Integer> integerSet = new HashSet<>();
+			Set<String> stringSet = permutations(digitsCopy, size);
+			for (String str : stringSet) {
+				integerSet.add(Integer.parseInt(str));
+			}
+			return integerSet;
+		}
+
 		//gets a set of numbers that are permutations of this current number
 		//e.g. if this number were 13 and the size given is 3,
 		//it would return 13, 31, 103, 130, etc.
-		private Set<Integer> permutations(int size) {
-			//todo
-			return null;
+		//This also needs to return Strings so that zeroes in the middle of a number
+		//don't get erased by Integer.parseInt().
+		private Set<String> permutations(Map<Character, Integer> tally, int size) {
+			Set<String> set = new HashSet<>();
+			//base case
+			if (size == 1) {
+				for (Map.Entry<Character, Integer> entry : tally.entrySet()) {
+					set.add(entry.getKey().toString());
+				}
+			}
+			else {
+				//getting downstream permutations, then prefixing the current character
+				for (Map.Entry<Character, Integer> entry : tally.entrySet()) {
+					Map<Character, Integer> tallyCopy = new HashMap<>(tally);
+					if (entry.getValue() == 1) {
+						tallyCopy.remove(entry.getKey());
+					}
+					else {
+						tallyCopy.replace(entry.getKey(), entry.getValue() - 1);
+					}
+					Set<String> partialResult = permutations(tallyCopy, size - 1);
+					for (String str : partialResult) {
+						set.add(str + entry.getKey());
+					}
+				}
+			}
+			return set;
 		}
 
 		@Override
